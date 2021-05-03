7ed596dd4ab362ac33b7e0ec5c77479bc54df4c5
Vladislav Varako <pilotvarako@gmail.com>
2021-05-03 20:08:59 +0600

    Add index and config project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>E-skidka</title>
+        <link rel="stylesheet" type="text/css" href="css/header.css"/>
+    </head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="/"><img src="img/logo.svg" alt="Logo"/></a>
+        </div>
+        <div class="button_entry">
+            <p><button>Вход</button></p>
+        </div>
+        <div class="button_registry">
+            <p><button>Регистрация</button></p>
+        </div>
+        <div class="line">
+            <hr>
+        </div>
+        <nav>
+            <div class="header_menu">
+                <ul>
+                    <li><a href="/coupons">Купоны</a></li>
+                    <li><a href="/smartForm">Smart форма</a></li>
+                    <li><a href="/">О сервисе</a></li>
+                    <li><a href="http://mnogo-skidok.ru/"><img src="img/mnogo_skidok.svg" alt="Mnogo-skidok"/></a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
