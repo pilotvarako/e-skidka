59de6780d5e258361c4b51b899615ae8370e274c
Vladislav Varako <pilotvarako@gmail.com>
2021-05-06 22:27:48 +0600

    add header_authorization

---
+++ resources/views/header_authorization.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>E-skidka</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css"/>
+</head>
+<body>
+<header>
+    <div class="logo">
+        <a href="/"><img src="img/logo.svg" alt="Logo"/></a>
+    </div>
+    <div class="user_profile">
+        <a href="/"><img src="img/user_auth_logo.svg" alt="Profile"/></a>
+    </div>
+    <div class="exit_profile">
+        <a href="/"><img src="img/exit_logo.svg" alt="Exit"/></a>
+    </div>
+    <div class="line">
+        <hr>
+    </div>
+    <nav>
+        <div class="header_menu">
+            <ul>
+                <a href="/coupons"><li><img src="img/coupon_logo.svg" alt="Logo"/><p>Купоны</p></li></a>
+                <a href="/"><li><img src="img/smart_form_logo.svg" alt="Logo"/><p>Smart форма</p></li></a>
+                <a href="/"><li><img src="img/service_logo.svg" alt="Logo"/><p>О сервисе</p></li></a>
+            </ul>
+        </div>
+        <div class="mnogo_skidok">
+            <a href="http://mnogo-skidok.ru/"><img src="img/mnogo_skidok.svg" alt="Mnogo-skidok"/></a>
+        </div>
+    </nav>
+</header>
+</body>
+</html>
