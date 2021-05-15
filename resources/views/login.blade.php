de0ace3ae8ed5961ec6fe609c4b531549ad051bd
Vladislav Varako <pilotvarako@gmail.com>
2021-05-15 22:36:24 +0600

    create view login and edit css, add logo

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('title', 'Авторизация')
+
+@section('content')
+    <div class="login_scope">
+        <div class="title_logo"><img src="img/auth/login_logo.svg" alt="Login logo"/></div>
+        <div class="title_name"><h2>Войти в аккаунт</h2></div>
+        <div class="form_scope_login">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="email"><input type="email" placeholder="Эл.почта" name="email" required /></div>
+                <div class="password"><input type="password" placeholder="Пароль" name="password" required /></div>
+                <div class="login_confirm"><button type="submit">Войти</button></div>
+            </form>
+        </div>
+    </div>
+@endsection
