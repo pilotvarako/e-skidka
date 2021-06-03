85b8db777e2c47475edeede46b669f0296200efd
Vladislav Varako <pilotvarako@gmail.com>
2021-06-04 00:40:18 +0600

    edit view for smart_form and css file

--- resources/views/smart_form/edit.blade.php
+++ resources/views/smart_form/edit.blade.php
@@ -12,11 +12,19 @@
             <div class="item_question">
                 @csrf
                 <div class="content"><p>test</p></div>
-                <div class="positive_answer"><input type="radio" value="positive" name="test"/>positive</div>
-                <div class="negative_answer"><input type="radio" value="negative" name="test"/>negative</div>
+                <div class="positive_answer">
+                    <input id="positive" type="radio" value="positive" name="test" required/>
+                    <label for="positive">positive</label>
+                </div>
+                <div class="negative_answer">
+                    <input id="negative" type="radio" value="negative" name="test" required/>
+                    <label for="negative">negative</label>
+                </div>
             </div>
-            <div class="smart_form_confirm"><button type="submit">Применить</button></div>
-            <div class="smart_form_cancel"><button type="reset">Отмена</button></div>
+            <div class="smart_form_send_scope">
+                <div class="smart_form_confirm"><button type="submit">Применить</button></div>
+                <div class="smart_form_cancel"><button type="reset">Отмена</button></div>
+            </div>
         </form>
     </div>
 @endsection
