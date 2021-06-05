e65f342eebaa8ec92c0837fbb598f5cde59a0938
Vladislav Varako <pilotvarako@gmail.com>
2021-06-05 20:02:21 +0600

    add adaptive design for computers

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0">
     @stack('meta')
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/base.css') }}"/>
